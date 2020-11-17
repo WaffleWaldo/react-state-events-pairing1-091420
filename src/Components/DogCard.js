d389b64bef3d60da22b9b441467757d0aa845075
WaffleWaldo <oswaldoortiz0519@gmail.com>
2020-11-17 10:37:34 -0500

    Almost Done.

--- src/Components/DogCard.js
+++ src/Components/DogCard.js
@@ -1,17 +1,48 @@
 import React from "react";
 
-function DogCard() {
-  return (
-    <div className="card">
-      <span className="content">
-        <h2 >{/*Dog name goes here*/}</h2>
-        <img alt="" src={""} />
-      </span>
-      <span className="bark">
-        <button>Bark</button>
-      </span>
-    </div>
-  );
-}
 
+// function DogCard(props) {
+//   return (
+//     <div className="card">
+//       <span className="content">
+//         <h2 >{props.name}</h2>
+//         <img alt={props.breed} src={props.img} />
+//       </span>
+//       <span className="bark">
+//         <button>Bark</button>
+//       </span>
+//     </div>
+//   );
+// }
+class DogCard extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {barkClicked: false}
+  }
+
+  handleClick = () => {
+    this.setState(previousState => {
+      return {
+        barkClicked: !previousState.barkClicked
+      }
+    })
+  }
+
+
+  render() {
+    return (
+      <div className="card">
+        <span className="content">
+          <h2 >{this.props.name}</h2>
+          <img alt={this.props.breed} src={this.props.img} />
+        </span>
+        <span className="bark">
+          <button onClick={this.handleClick}>Bark</button>
+          
+        </span>
+      </div>
+    );
+  }
+}
 export default DogCard;
--- src/Containers/DogsList.js
+++ src/Containers/DogsList.js
@@ -1,8 +1,26 @@
 import React, { Component } from "react";
+import DogCard from "../Components/DogCard"
+import apiResponse from "../api"
+
+// console.log(apiResponse.apiResponse[0])
+// console.log(typeof apiResponse)
+
+// const api = apiResponse
+
+// console.log(api.map(dog => dog.name))
+
+
+
+// renderApi()
 
 class DogsList extends Component {
+  
+  renderApi = () => {
+    return apiResponse.apiResponse.map(dog => <DogCard name={dog.name} img={dog.img} key={dog.id} breed={dog.breed} />)
+  }
+
   render() {
-    return <div className="list">{/*list of dogs go here*/}</div>;
+    return <div className="list">{this.renderApi()}</div>;
   }
 }
 
